071eeaf48482e584e6d9a00e8fbdcd60996da5a7
Yinling <yinling@vmaxx.co>
2018-04-19 12:23:49 -0700

    Add files via upload

---
+++ create_genderracesh.sh
@@ -0,0 +1,74 @@
+cp mm.sh ff.sh
+sed -i 's/M-/F-/g' ff.sh
+sed -i 's/male/woman/g' ff.sh
+sed -i 's/mm_p/ff_p/g' ff.sh
+cp mm.sh fm.sh
+sed -i 's/M-/F-/g' fm.sh
+sed -i 's/mm_p/fm_p/g' fm.sh
+cp mm.sh mf.sh
+sed -i 's/male/woman/g' mf.sh
+sed -i 's/mm_p/mf_p/g' mf.sh
+cp mm.sh ww_p.sh
+sed -i 's/M-/CFD-W/g' ww_p.sh
+sed -i 's/male/white/g' ww_p.sh
+sed -i 's/mm_p/ww_p/g' ww_p.sh
+cp mm.sh wb_p.sh
+sed -i 's/M-/CFD-W/g' wb_p.sh
+sed -i 's/male/black/g' wb_p.sh
+sed -i 's/mm_p/wb_p/g' wb_p.sh
+cp mm.sh wh_p.sh
+sed -i 's/M-/CFD-W/g' wh_p.sh
+sed -i 's/male/hispanic/g' wh_p.sh
+sed -i 's/mm_p/wh_p/g' wh_p.sh
+cp mm.sh wa_p.sh
+sed -i 's/M-/CFD-W/g' wa_p.sh
+sed -i 's/male/asia/g' wa_p.sh
+sed -i 's/mm_p/wa_p/g' wa_p.sh
+cp mm.sh bw_p.sh
+sed -i 's/M-/CFD-B/g' bw_p.sh
+sed -i 's/male/white/g' bw_p.sh
+sed -i 's/mm_p/bw_p/g' bw_p.sh
+cp mm.sh bb_p.sh
+sed -i 's/M-/CFD-B/g' bb_p.sh
+sed -i 's/male/black/g' bb_p.sh
+sed -i 's/mm_p/bb_p/g' bb_p.sh
+cp mm.sh bh_p.sh
+sed -i 's/M-/CFD-B/g' bh_p.sh
+sed -i 's/male/hispanic/g' bh_p.sh
+sed -i 's/mm_p/bh_p/g' bh_p.sh
+cp mm.sh ba_p.sh
+sed -i 's/M-/CFD-B/g' ba_p.sh
+sed -i 's/male/asia/g' ba_p.sh
+sed -i 's/mm_p/ba_p/g' ba_p.sh
+cp mm.sh hw_p.sh
+sed -i 's/M-/CFD-L/g' hw_p.sh
+sed -i 's/male/white/g' hw_p.sh
+sed -i 's/mm_p/hw_p/g' hw_p.sh
+cp mm.sh hb_p.sh
+sed -i 's/M-/CFD-L/g' hb_p.sh
+sed -i 's/male/black/g' hb_p.sh
+sed -i 's/mm_p/hb_p/g' hb_p.sh
+cp mm.sh hh_p.sh
+sed -i 's/M-/CFD-L/g' hh_p.sh
+sed -i 's/male/hispanic/g' hh_p.sh
+sed -i 's/mm_p/hh_p/g' hh_p.sh
+cp mm.sh ha_p.sh
+sed -i 's/M-/CFD-L/g' ha_p.sh
+sed -i 's/male/asia/g' ha_p.sh
+sed -i 's/mm_p/ha_p/g' ha_p.sh
+cp mm.sh aw_p.sh
+sed -i 's/M-/CFD-A/g' aw_p.sh
+sed -i 's/male/white/g' aw_p.sh
+sed -i 's/mm_p/aw_p/g' aw_p.sh
+cp mm.sh ab_p.sh
+sed -i 's/M-/CFD-A/g' ab_p.sh
+sed -i 's/male/black/g' ab_p.sh
+sed -i 's/mm_p/ab_p/g' ab_p.sh
+cp mm.sh ah_p.sh
+sed -i 's/M-/CFD-A/g' ah_p.sh
+sed -i 's/male/hispanic/g' ah_p.sh
+sed -i 's/mm_p/ah_p/g' ah_p.sh
+cp mm.sh aa_p.sh
+sed -i 's/M-/CFD-A/g' aa_p.sh
+sed -i 's/male/asia/g' aa_p.sh
+sed -i 's/mm_p/aa_p/g' aa_p.sh
---
+++ source_genderrace.sh
@@ -0,0 +1,22 @@
+source mm.sh
+source ff.sh
+source fm.sh
+source mf.sh
+paste ff_p.txt fm_p.txt mm_p.txt mf_p.txt >gender.out
+source aa_p.sh
+source ah_p.sh
+source ab_p.sh
+source aw_p.sh
+source ha_p.sh
+source hh_p.sh
+source hb_p.sh
+source hw_p.sh
+source wa_p.sh
+source wh_p.sh
+source wb_p.sh
+source ww_p.sh
+source ba_p.sh
+source bh_p.sh
+source bb_p.sh
+source bw_p.sh
+paste ww_p.txt wb_p.txt wh_p.txt wa_p.txt bw_p.txt bb_p.txt bh_p.txt ba_p.txt hw_p.txt hb_p.txt hh_p.txt ha_p.txt aw_p.txt ab_p.txt ah_p.txt aa_p.txt >race.out
